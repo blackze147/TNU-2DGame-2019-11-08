febf41a3e42634f1cc1909ecd9a702d8ef52490e
blackze147 <56988699+blackze147@users.noreply.github.com>
2019-11-29 11:09:35 +0800

    20191108

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 定義欄位 Field
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結尾
+    // private 私人 (在屬性面板上隱藏) 預設
+    // public 公開 (在屬性面板上顯示)
+    [Header("速度"), Range(0f, 100f)]
+    public float speed = 3.5f;          // 浮點數 - 結尾要有 f
+    [Header("跳躍"), Range(100, 2000)]
+    public int jump = 300;              // 整數
+    [Header("是否在地板上"), Tooltip("用來判定角色是否在地板上。")]
+    public bool isGround = false;       // 布林值 - true、false
+    [Header("角色名稱")]
+    public string _name = "KID";        // 字串 - 需要用雙引號
+}
